bd3f424b56256b441825d4b770383e63827101ac
Wesley Clements <wesley.clements@gmail.com>
2022-07-07 16:00:49 -0400

    added config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base-typescript',
+  ],
+  ignorePatterns: ['*.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    "func-names": ["error", "always", { "generators": "never" }]
+  },
+};
